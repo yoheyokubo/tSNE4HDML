9b449ef0adcc8d07086d11d4aabc3b90c845e523
YoheiOkubo <45516688+yoheyokubo@users.noreply.github.com>
2024-05-24 01:16:51 +0900

    Update train_tsne.sh

--- train_tsne.sh
+++ train_tsne.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#$ -S /bin/bash
-#$ -cwd
-#$ -l v100=1,s_vmem=25G
-#$ -pe def_slot 4
 
 lr=1e-3
 lr_VQ=1e-3
@@ -11,9 +7,9 @@ bad_limit=0
 batch_size=128
 margin=1
 
-model_save_path="model_save_path_v7"
+model_save_path="model_save_path"
 device="cuda:0"
-CODE="code_classification/train.py"
+CODE="code/train.py"
 
 kmer=6
 model="Encoder"
@@ -25,7 +21,7 @@ strategy_emd='tsne'
 strategy_samp='random'
 strategy_rank='dist'
 strategy_perp=5
-strategy_gamm=1.0
+strategy_gamm=0.01
 strategy_hrms_alph=2
 strategy_hrms_beta=2
 strategy_hrms_gamm=1
@@ -39,7 +35,6 @@ id2acc='data/host_accs.json'
 scientific_names='data/scientific_names.json'
 lineages='data/lineages.json'
 
-module load /usr/local/package/modulefiles/python/3.12.0
 python3 $CODE --model $model --model_backbone $model_backbone --model_dir $model_save_path --seed $seed --kmer $kmer --margin $margin \
     --device $device --lr $lr --lr_VQ $lr_VQ --epoch $epoch --batch_size $batch_size --latent_channels $latent_channels --latent_space $latent_space --gamma $gamma\
     --strategy_emd $strategy_emd --strategy_samp $strategy_samp --strategy_rank $strategy_rank --strategy_perp $strategy_perp \
